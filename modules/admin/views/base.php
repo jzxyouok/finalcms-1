6090c40ee7da2ee8c716a1b1eb99040fca44fdce
jun <fu9068@163.com>
2016-07-23 12:52:09 +0800

    init

--- modules/admin/views/base.php
+++ modules/admin/views/base.php
@@ -1,7 +1,13 @@
 
-
 <?php $this->beginBlock('left'); ?>
 
-<?php include(Yii::getAlias('@app/modules/admin/views/left.php')); ?>
+<?php
+$leftView = 'left' . ucfirst($this->context->id);
+$leftViewFile = Yii::getAlias('@app/modules/admin/views/'.$leftView.'.php');
+$leftViewExists = file_exists($leftViewFile);
+if ($leftViewExists) {
+    include($leftViewFile);
+}
+?>
 
 <?php $this->endBlock(); ?>
