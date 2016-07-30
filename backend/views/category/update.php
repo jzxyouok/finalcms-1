9b616a2532252ad84b25236f90df0db86bb0b2b5
jun <fu9068@163.com>
2016-07-30 21:10:33 +0800

    category search

--- backend/views/category/update.php
+++ backend/views/category/update.php
@@ -9,7 +9,7 @@ include(Yii::getAlias('@backend/views/base.php'));
 
 $this->title = '更新栏目: ' . $model->id;
 $this->params['breadcrumbs'][] = ['label' => '栏目管理', 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
+$this->params['breadcrumbs'][] = ['label' => \common\services\Category::cateName($model->id), 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = '更新栏目';
 ?>
 <div class="category-update">
